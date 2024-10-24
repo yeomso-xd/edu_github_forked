aed7719126791863563d2dc9ecfde10875daa29b
unknown <charsyam@gmail.com>
2024-10-25 01:22:36 +0900

    add add function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('calu.html', result=0)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    num1 = int(request.form['num1'])
+    num2 = int(request.form['num2'])
+
+    return render_template('calu.html', result=(num1+num2))
 
 if __name__ == '__main__':
     app.run(debug=True)
